ab8775d0f228b8735d0085f5cd194f0e3fcd85da
Diego Fernandes <fernandes.profissional@hotmail.com>
2019-08-02 16:38:14 -0300

    ajustes, inserindo favicon, arq sql insert cidades

---
+++ scrpt_insert_cidade.sql
@@ -0,0 +1,159 @@
+insert into Cidade values (1,'Rio Branco');
+insert into Cidade values (1,'Cruzeiro do Sul');
+insert into Cidade values (1,'Porto Acre');
+insert into Cidade values (1,'Feijó');
+insert into Cidade values (1,'Jordão');
+
+insert into Cidade values (2,'Maceió');
+insert into Cidade values (2,'Penedo');
+insert into Cidade values (2,'Rio Largo');
+insert into Cidade values (2,'Piranhas');
+insert into Cidade values (2,'Pilar');
+
+insert into Cidade values (3,'Cutias');
+insert into Cidade values (3,'Ferreira Gomes');
+insert into Cidade values (3,'Itaubal');
+insert into Cidade values (3,'Laranjal do Jari');
+insert into Cidade values (3,'Macapa');
+
+insert into Cidade values (4,'Manaus');
+insert into Cidade values (4,'Borba');
+insert into Cidade values (4,'Alvaraes');
+insert into Cidade values (4,'Anori');
+insert into Cidade values (4,'Beruri');
+
+insert into Cidade values (5,'Salvador');
+insert into Cidade values (5,'Porto Seguro');
+insert into Cidade values (5,'Juazeiro');
+insert into Cidade values (5,'Vitória da Conquista');
+insert into Cidade values (5,'Cachoeira');
+
+insert into Cidade values (6,'Fortaleza');
+insert into Cidade values (6,'Sobral');
+insert into Cidade values (6,'Juazeiro do Norte');
+insert into Cidade values (6,'Crato');
+insert into Cidade values (6,'Aracati');
+
+insert into Cidade values (7,'Brasilia');
+insert into Cidade values (7,'Sobradinho');
+insert into Cidade values (7,'Gama');
+insert into Cidade values (7,'Cristalina');
+insert into Cidade values (7,'Cidade ocidental');
+
+insert into Cidade values (8,'Serra');
+insert into Cidade values (8,'São Mateus');
+insert into Cidade values (8,'Vitoria');
+insert into Cidade values (8,'Guarapari');
+insert into Cidade values (8,'Cachoeira do Itapemirim');
+
+insert into Cidade values (9,'Trindade');
+insert into Cidade values (9,'Jataí');
+insert into Cidade values (9,'Pirenópolis');
+insert into Cidade values (9,'Catalão');
+insert into Cidade values (9,'Goiânia');
+
+insert into Cidade values (10,'Imperatriz');
+insert into Cidade values (10,'São Luís');
+insert into Cidade values (10,'Caxias');
+insert into Cidade values (10,'Codó');
+insert into Cidade values (10,'Santa Inês');
+
+insert into Cidade values (11,'Sorriso');
+insert into Cidade values (11,'Cuiabá');
+insert into Cidade values (11,'Sinop');
+insert into Cidade values (11,'Barra do Garças');
+insert into Cidade values (11,'Poconé');
+
+insert into Cidade values (12,'Corumbá');
+insert into Cidade values (12,'Bonito');
+insert into Cidade values (12,'Coxim');
+insert into Cidade values (12,'Ponta Porã');
+insert into Cidade values (12,'Três lagoas');
+
+insert into Cidade values (13,'Ouro Preto');
+insert into Cidade values (13,'Brumadinho');
+insert into Cidade values (13,'Belo Horizonte');
+insert into Cidade values (13,'Juiz de Fora');
+insert into Cidade values (13,'Tiradentes');
+
+insert into Cidade values (14,'Belém');
+insert into Cidade values (14,'Santarém');
+insert into Cidade values (14,'Castanhal');
+insert into Cidade values (14,'Moju');
+insert into Cidade values (14,'Bragança');
+
+insert into Cidade values (15,'João Pessoa');
+insert into Cidade values (15,'Santa Rita');
+insert into Cidade values (15,'Bananeiras');
+insert into Cidade values (15,'Campina Grande');
+insert into Cidade values (15,'Queimadas');
+
+insert into Cidade values (16,'Curitiba');
+insert into Cidade values (16,'Foz do Iguaçu');
+insert into Cidade values (16,'Londrina');
+insert into Cidade values (16,'Pato Branco');
+insert into Cidade values (16,'Ponta Grossa');
+
+insert into Cidade values (17,'Recife');
+insert into Cidade values (17,'Olinda');
+insert into Cidade values (17,'Petrolina');
+insert into Cidade values (17,'Gravatá');
+insert into Cidade values (17,'Caruaru');
+
+insert into Cidade values (18,'Parnaíba');
+insert into Cidade values (18,'Teresina');
+insert into Cidade values (18,'Pedro II');
+insert into Cidade values (18,'Picos');
+insert into Cidade values (18,'Oeiras');
+
+insert into Cidade values (19,'Niterói');
+insert into Cidade values (19,'Petrópolis');
+insert into Cidade values (19,'Cabo Frio');
+insert into Cidade values (19,'Armação dos Buzios');
+
+insert into Cidade values (20,'Natal');
+insert into Cidade values (20,'Macau');
+insert into Cidade values (20,'Pau dos Ferros');
+insert into Cidade values (20,'Touros');
+insert into Cidade values (20,'Tibau');
+
+insert into Cidade values (21,'Porto Alegre');
+insert into Cidade values (21,'Rio Grande');
+insert into Cidade values (21,'Caxias do Sul');
+insert into Cidade values (21,'Gramado');
+insert into Cidade values (21,'Pelotas');
+
+insert into Cidade values (22,'Porto Velho');
+insert into Cidade values (22,'Ouro Preto do Oeste');
+insert into Cidade values (22,'Cerejeiras');
+insert into Cidade values (22,'Urupá');
+insert into Cidade values (22,'Cabixi');
+
+insert into Cidade values (23,'Boa Vista');
+insert into Cidade values (23,'Bonfim');
+insert into Cidade values (23,'Alto Alegre');
+insert into Cidade values (23,'Pacaraima');
+insert into Cidade values (23,'Amajari');
+
+insert into Cidade values (24,'Florianópolis');
+insert into Cidade values (24,'Chapecó');
+insert into Cidade values (24,'Blumenau');
+insert into Cidade values (24,'Itajaí');
+insert into Cidade values (24,'Joinville');
+
+insert into Cidade values (25,'Campinas');
+insert into Cidade values (25,'Santos');
+insert into Cidade values (25,'Franca');
+
+insert into Cidade values (26,'Aracaju');
+insert into Cidade values (26,'Largato');
+insert into Cidade values (26,'Estância');
+insert into Cidade values (26,'Laranjeiras');
+insert into Cidade values (26,'São Cristovão');
+
+insert into Cidade values (27,'Palmas');
+insert into Cidade values (27,'Gurupi');
+insert into Cidade values (27,'Araguaína');
+insert into Cidade values (27,'Paraíso do Tocantins');
+insert into Cidade values (27,'Almas');
+
